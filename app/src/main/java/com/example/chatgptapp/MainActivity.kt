c543c933b4ae7014d16f059023aaa0e44f991f63
Hasan Cihad Altay <caltayzzz@gmail.com>
2023-06-29 17:42:45 +0300

    google speech to text added. next will be chatgpt api configuration

--- app/src/main/java/com/example/chatgptapp/MainActivity.kt
+++ app/src/main/java/com/example/chatgptapp/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.chatgptapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils.replace
 import android.widget.Button
 import androidx.activity.viewModels
 import androidx.fragment.app.commit
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: MainViewModel by viewModels<MainViewModel>()
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +24,12 @@ class MainActivity : AppCompatActivity() {
         val askButton: Button = findViewById(R.id.ask_button)
 
         askButton.setOnClickListener {
-            viewModel.showAskAlertDialog(this)
+            viewModel.askSpeechInput(this)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        viewModel.handleSpeechResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/com/example/chatgptapp/MainViewModel.kt
+++ app/src/main/java/com/example/chatgptapp/MainViewModel.kt
@@ -1,31 +1,41 @@
 package com.example.chatgptapp
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.util.Log
+import android.view.Gravity
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.ViewModel
+import java.util.Locale
 
 class MainViewModel : ViewModel() {
-    // ViewModel ile ilgili veriler ve işlevler buraya yerleştirilebilir
 
-    fun showAskAlertDialog(context: Context) {
-        val alertDialog = AlertDialog.Builder(context)
-            .setTitle("Ask")
-            .setMessage("How can I help you?")
-            .setPositiveButton("OK") { dialog, which ->
-                // OK butonuna tıklanınca yapılacak işlemler
-            }
-            .setNegativeButton("Cancel") { dialog, which ->
-                // Cancel butonuna tıklanınca yapılacak işlemler
-            }
-            .create()
+    private val RQ_SPEECH_REC = 102
 
-        val layoutParams = alertDialog.window?.attributes
-        layoutParams?.horizontalMargin = context.resources.getDimensionPixelSize(R.dimen.dialog_margin).toFloat()
-        layoutParams?.verticalMargin = context.resources.getDimensionPixelSize(R.dimen.dialog_margin).toFloat()
-        alertDialog.window?.attributes = layoutParams
-
-        alertDialog.show()
-        alertDialog.window?.attributes = layoutParams
+    fun askSpeechInput(context: Context) {
+        if(!SpeechRecognizer.isRecognitionAvailable(context)) {
+            Toast.makeText(context, "Speech Recognition not available", Toast.LENGTH_SHORT).show()
+        } else {
+            val i = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Ask AI")
+            startActivityForResult(context as Activity, i, RQ_SPEECH_REC, null)
+        }
     }
+
+    fun handleSpeechResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == RQ_SPEECH_REC && resultCode == Activity.RESULT_OK) {
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            Log.d("Mesaj: ", result?.get(0).toString())
+        }
+    }
+
 }
